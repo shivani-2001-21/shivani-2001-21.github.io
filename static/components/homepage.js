d79884fdbedc62f85117b84d706f166a11bce4e2
shivani-2001-21 <78609586+shivani-2001-21@users.noreply.github.com>
2024-03-07 15:57:29 +0530

    Create homepage.js

---
+++ static/components/homepage.js
@@ -0,0 +1,16 @@
+export default {
+    template:`
+    <div>   
+        <br>
+        <center> 
+        <h3> Hi! Welcome to Dashboard </h3>
+        </center>
+         
+
+      <!--  <router-link to="/export_data"> Export Data </router-link> -->
+
+    <h3><div> Hello </div>
+        
+              
+    </div>
+    `}
